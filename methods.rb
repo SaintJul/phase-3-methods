1908a4d901b05c3f3513c50672ee1591c46e901e
Julius.M <favor.julius@gmail.com>
2021-11-19 17:42:35 -0500

    commit tree a5907db7babcad2d70074e75949fb035709dafb4

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet("John")
+
+def greet_with_default name = "programmer"
+    p "Hello, #{name}!"
+end
+
+greet_with_default("David")
+
+def add num1, num2
+    p num1 + num2
+end
+
+add(4,5)
+
+def halve num 
+    return nil if num.class != Integer 
+    p num/2
+end
+
+halve(100)
